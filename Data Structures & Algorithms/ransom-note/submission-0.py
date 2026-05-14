49e22ca68f6e2c0e5ae2e024bf447d376ff15a43
Ashish Kumar <ashish.888282@gmail.com>
2026-05-13 22:29:05 -0700

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        countRansom = {}
+        countMag = {}
+
+        for r in ransomNote:
+            countRansom[r] = countRansom.get(r, 0) + 1
+        
+        for m in magazine:
+            countMag[m] = countMag.get(m, 0) + 1
+        
+        print(countRansom)
+        print(countMag)
+        for key, val in countRansom.items():
+            val_m = countMag.get(key, 0)
+
+            if val_m < val:
+                return False
+        
+        return True
