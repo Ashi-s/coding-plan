28e5bbb68cc57a4974107e5875c0a462ccaaddc6
Ashish Kumar <ashish.888282@gmail.com>
2026-05-26 19:08:16 -0700

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = { c:[] for c in range(numCourses) }
+
+        for a, b in prerequisites:
+            adj[a].append(b)
+
+        def helper(crs):
+            if crs in processed:
+                return True
+            
+            if crs in visited: # cycle
+                return False
+            
+            visited.add(crs)
+            for p in adj[crs]:
+                if not helper(p):
+                    return False
+            
+            processed.add(crs)
+            visited.remove(crs)
+            output.append(crs)
+            return True
+
+
+        visited = set()
+        processed = set()
+        output = []
+        for crs in range(numCourses):
+            if not helper(crs):
+                return []
+        
+        return output
